a9a973fffbf319c8cac8de28877cfdc8ab9d3900
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2019-11-09 17:06:49 +0530

    Test

--- src/main/java/net/guides/todo/repositories/TodoRepository.java
+++ src/main/java/net/guides/todo/repositories/TodoRepository.java
@@ -2,8 +2,11 @@ package net.guides.todo.repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import net.guides.todo.models.TodoModel;
 
+@Repository
 public interface TodoRepository extends JpaRepository < TodoModel, Long > {
 	List <TodoModel> findByUserName(String user);
 }
